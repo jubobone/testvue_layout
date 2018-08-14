a44c121fda75c31e7e2b1ff62846eea2f04b9c45
Thaweepol N <thaweepol.nuangnong@gmail.com>
2018-08-14 23:54:36 +0700

    add bootstrap-vue plugin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <appHeader></appHeader>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/shared/Header.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    appHeader: Header
   }
 }
 </script>
